43195458260f9a353e31ef94a58b1fd9296a945f
matty199905 <matty199905@gmail.com>
2025-10-28 12:58:34 -0300

    add firebase

--- src/Firebase/firebaseConfig.ts
+++ src/Firebase/firebaseConfig.ts
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { enableIndexedDbPersistence } from "firebase/firestore";
+import { getFirestore } from "firebase/firesto
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
