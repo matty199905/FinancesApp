63e62f852316d405790e73e43cb1c14085adf621
matty199905 <matty199905@gmail.com>
2025-11-03 13:53:07 -0300

    .env.local added

--- src/Firebase/firebaseConfig.ts
+++ src/Firebase/firebaseConfig.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCQ3tYPd34U7tl9oMmbit4cX1bbEgc4k_4",
-  authDomain: "financesapp-7043f.firebaseapp.com",
-  projectId: "financesapp-7043f",
-storageBucket: "financesapp-7043f.appspot.com",
-  messagingSenderId: "461041499733",
-  appId: "1:461041499733:web:cce29874ea608bd2e59edc",
-  measurementId: "G-5F5CL7MNS2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
